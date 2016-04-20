34cd55d6723ee3922d2b423b29ae216a3e05fcb7
Irene De La Torre <irene.delatorre@gmail.com>
2016-04-20 12:00:28 -0400

    tooltip in chrome

--- sketch3.js
+++ sketch3.js
@@ -352,7 +352,7 @@ function tooltip() {
         }
     } else {
         if (mouseX > 0 && mouseX <= 730) {
-            tooltip.position(mouseX + 220, mouseY + 300)
+            tooltip.position(mouseX + 400, mouseY + 300)
         } else {
             tooltip.position(mouseX + 10, mouseY + 300)
         }
