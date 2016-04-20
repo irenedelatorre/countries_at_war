d5f83278ea77969c375c47e55354c0dacae3314d
Irene De La Torre <irene.delatorre@gmail.com>
2016-04-20 12:03:10 -0400

    tooltip in mozilla

--- sketch3.js
+++ sketch3.js
@@ -346,7 +346,7 @@ function tooltip() {
 
     if (!is_chrome) {
         if (mouseX > 0 && mouseX <= 730) {
-            tooltip.position(mouseX + 400, mouseY + 300)
+            tooltip.position(mouseX + 10, mouseY + 300)
         } else {
             tooltip.position(mouseX - 300, mouseY + 300)
         }
@@ -354,7 +354,7 @@ function tooltip() {
         if (mouseX > 0 && mouseX <= 730) {
             tooltip.position(mouseX + 800, mouseY + 300)
         } else {
-            tooltip.position(mouseX + 10, mouseY + 300)
+            tooltip.position(mouseX - 10, mouseY + 300)
         }
     }
 
